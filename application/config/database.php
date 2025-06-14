ca615ecd5996077ed9d7d1c78d56bc2c15193ad0
Your Name <you@example.com>
2025-06-14 16:56:15 +0530

    Live 14-06-2025

--- application/config/database.php
+++ application/config/database.php
@@ -73,34 +73,11 @@ defined('BASEPATH') or exit('No direct script access allowed');
 $active_group = 'default';
 $query_builder = TRUE;
 
-
-// $db['default'] = array(
-// 	'dsn'	=> '',
-// 	'hostname' => 'TMS-CO',
-// 	'username' => 'sa',
-// 	'password' => 'cosa@123',
-// 	'database' => 'PRECOT_ERP_HR',
-// 	'dbdriver' => 'sqlsrv',
-// 	'dbprefix' => '',
-// 	'pconnect' => FALSE,
-// 	'db_debug' => (ENVIRONMENT !== 'production'),
-// 	'cache_on' => FALSE,
-// 	'cachedir' => '',
-// 	'char_set' => 'utf8',
-// 	'dbcollat' => 'utf8_general_ci',
-// 	'swap_pre' => '',
-// 	'encrypt' => FALSE,
-// 	'compress' => FALSE,
-// 	'stricton' => FALSE,
-// 	'failover' => array(),
-// 	'save_queries' => TRUE
-// );
-
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'SCOTO-PHP',
+	'hostname' => 'TMS-CO',
 	'username' => 'sa',
-	'password' => 'SCOTO@2025',
+	'password' => 'cosa@123',
 	'database' => 'PRECOT_ERP_HR',
 	'dbdriver' => 'sqlsrv',
 	'dbprefix' => '',
@@ -117,6 +94,28 @@ $db['default'] = array(
 	'failover' => array(),
 	'save_queries' => TRUE
 );
+
+// $db['default'] = array(
+// 	'dsn'	=> '',
+// 	'hostname' => 'SCOTO-PHP',
+// 	'username' => 'sa',
+// 	'password' => 'SCOTO@2025',
+// 	'database' => 'PRECOT_ERP_HR',
+// 	'dbdriver' => 'sqlsrv',
+// 	'dbprefix' => '',
+// 	'pconnect' => FALSE,
+// 	'db_debug' => (ENVIRONMENT !== 'production'),
+// 	'cache_on' => FALSE,
+// 	'cachedir' => '',
+// 	'char_set' => 'utf8',
+// 	'dbcollat' => 'utf8_general_ci',
+// 	'swap_pre' => '',
+// 	'encrypt' => FALSE,
+// 	'compress' => FALSE,
+// 	'stricton' => FALSE,
+// 	'failover' => array(),
+// 	'save_queries' => TRUE
+// );
 
 // $db['default'] = array(
 // 	'dsn'	=> '',
